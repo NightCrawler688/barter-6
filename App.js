f35ff115c81b5044af83fe2eb1a93a4b77274825
NightCrawler688 <yashprasad1205@gmail.com>
2020-12-15 19:00:26 +0530

    signup

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './screens/Welcome';
+console. disableYellowBox = true;
 
 export default class App extends React.Component{
   render(){
     return(
-      <View>
+      <View style = {styles.container}>
         <Welcome/>
       </View>
     )
   }
 }
+const styles = StyleSheet.create({ 
+  container: { flex: 1,  backgroundColor:'#F8BE85', alignItems: 'center', justifyContent: 'center', }, });
